61e194c8f5e98bc77fa60ed796e618f35b93b57a
cosyrusty <priyaanyadav@gmail.com>
2023-04-05 19:57:38 +0530

    rotate cube and project on 2d screen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::f32::consts::PI;
+
 use macroquad::prelude::*;
 
 fn window_conf() -> Conf {
@@ -12,31 +14,39 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    let mut cube = UnitCube::new();
-    // scale cube by 50x
-    cube.scale(50.);
-    // move cube to center of screen
-    cube.offset(vec3(screen_width() / 2., screen_height() / 2., 0.));
+    let cube = Cube::unit_cube();
+    let mut angle = 0.0;
 
     loop {
         if is_key_pressed(KeyCode::Escape) {
             break;
         }
+
+        // angle += 90 degree; each second.
+        angle = wrap_angle(angle + PI / 2. * get_frame_time());
+
         clear_background(WHITE);
-        cube.project();
+
+        // NOTE: offsetting cube changes its origin hence, rotation must be done before offset
+        cube.rotate_x(angle)
+            .rotate_y(angle)
+            .rotate_z(angle)
+            .scale(75.)
+            .offset(vec3(screen_width() / 2., screen_height() / 2., 0.))
+            .project();
+
         next_frame().await
     }
 }
 
-// length of each side is one
-pub struct UnitCube {
+struct Cube {
     vertices: [Vec3; 8],
 }
 
-impl UnitCube {
-    pub fn new() -> Self {
+impl Cube {
+    pub fn unit_cube() -> Self {
         #[rustfmt::skip]
-        let vertices: [Vec3; 8] = [
+        let vertices: [Vec3; 8]= [
             // front
             vec3(-0.5, -0.5, -0.5),
             vec3( 0.5, -0.5, -0.5),
@@ -48,18 +58,64 @@ impl UnitCube {
             vec3( 0.5,  0.5,  0.5),
             vec3(-0.5,  0.5,  0.5),
         ];
+
         Self { vertices }
     }
 
-    pub fn scale(&mut self, factor: f32) {
-        for v in &mut self.vertices {
-            *v *= factor;
+    pub fn scale(&self, factor: f32) -> Cube {
+        Self {
+            vertices: self.vertices.map(|v| v * factor),
         }
     }
 
-    pub fn offset(&mut self, value: Vec3) {
-        for v in &mut self.vertices {
-            *v += value;
+    pub fn offset(&self, value: Vec3) -> Cube {
+        Self {
+            vertices: self.vertices.map(|v| v + value),
+        }
+    }
+
+    pub fn rotate_x(&self, angle: f32) -> Cube {
+        // NOTE: origin of 'cube' must be x: 0.0, y: 0.0, z: 0.0;
+
+        #[rustfmt::skip]
+        let rotation_matrix = Mat3 {
+            x_axis: vec3(1.,           0.,          0.),
+            y_axis: vec3(0.,  angle.cos(), angle.sin()),
+            z_axis: vec3(0., -angle.sin(), angle.cos()),
+        };
+
+        Self {
+            vertices: self.vertices.map(|v| rotation_matrix * v),
+        }
+    }
+
+    pub fn rotate_y(&self, angle: f32) -> Cube {
+        // NOTE: origin of 'cube' must be x: 0.0, y: 0.0, z: 0.0;
+
+        #[rustfmt::skip]
+        let rotation_matrix = Mat3 {
+            x_axis: vec3(angle.cos(), 0., -angle.sin()),
+            y_axis: vec3(         0., 1.,           0.),
+            z_axis: vec3(angle.sin(), 0.,  angle.cos()),
+        };
+
+        Self {
+            vertices: self.vertices.map(|v| rotation_matrix * v),
+        }
+    }
+
+    pub fn rotate_z(&self, angle: f32) -> Cube {
+        // NOTE: origin of 'cube' must be x: 0.0, y: 0.0, z: 0.0;
+
+        #[rustfmt::skip]
+        let rotation_matrix = Mat3 {
+            x_axis: vec3( angle.cos(), angle.sin(), 0.),
+            y_axis: vec3(-angle.sin(), angle.cos(), 0.),
+            z_axis: vec3(          0.,          0., 1.),
+        };
+
+        Self {
+            vertices: self.vertices.map(|v| rotation_matrix * v),
         }
     }
 
@@ -70,29 +126,40 @@ impl UnitCube {
             z_axis: vec3(0., 0., 0.),
         };
 
-        let projected_vertices: [Vec3; 8] = self.vertices.map(|v| projection_matrix * v);
+        let projected_cube = self.vertices.map(|v| projection_matrix * v);
 
-        for v in projected_vertices {
+        for v in projected_cube {
             draw_circle(v.x, v.y, 3., RED);
         }
 
-        let v = projected_vertices;
+        let v = projected_cube;
         // front
-        draw_line(v[0].x, v[0].y, v[1].x, v[1].y, 2., RED);
-        draw_line(v[1].x, v[1].y, v[2].x, v[2].y, 2., RED);
-        draw_line(v[2].x, v[2].y, v[3].x, v[3].y, 2., RED);
-        draw_line(v[3].x, v[3].y, v[0].x, v[0].y, 2., RED);
+        draw_line(v[0].x, v[0].y, v[1].x, v[1].y, 3., RED);
+        draw_line(v[1].x, v[1].y, v[2].x, v[2].y, 3., RED);
+        draw_line(v[2].x, v[2].y, v[3].x, v[3].y, 3., RED);
+        draw_line(v[3].x, v[3].y, v[0].x, v[0].y, 3., RED);
 
         // back
-        draw_line(v[4].x, v[4].y, v[5].x, v[5].y, 2., RED);
-        draw_line(v[5].x, v[5].y, v[6].x, v[6].y, 2., RED);
-        draw_line(v[6].x, v[6].y, v[7].x, v[7].y, 2., RED);
-        draw_line(v[7].x, v[7].y, v[4].x, v[4].y, 2., RED);
+        draw_line(v[4].x, v[4].y, v[5].x, v[5].y, 3., RED);
+        draw_line(v[5].x, v[5].y, v[6].x, v[6].y, 3., RED);
+        draw_line(v[6].x, v[6].y, v[7].x, v[7].y, 3., RED);
+        draw_line(v[7].x, v[7].y, v[4].x, v[4].y, 3., RED);
 
         // side
-        draw_line(v[0].x, v[0].y, v[4].x, v[4].y, 2., RED);
-        draw_line(v[1].x, v[1].y, v[5].x, v[5].y, 2., RED);
-        draw_line(v[2].x, v[2].y, v[6].x, v[6].y, 2., RED);
-        draw_line(v[3].x, v[3].y, v[7].x, v[7].y, 2., RED);
+        draw_line(v[0].x, v[0].y, v[4].x, v[4].y, 3., RED);
+        draw_line(v[1].x, v[1].y, v[5].x, v[5].y, 3., RED);
+        draw_line(v[2].x, v[2].y, v[6].x, v[6].y, 3., RED);
+        draw_line(v[3].x, v[3].y, v[7].x, v[7].y, 3., RED);
+    }
+}
+
+pub fn wrap_angle(angle: f32) -> f32 {
+    if angle < 0.0 {
+        let dif = 0.0 - angle;
+        2. * PI - dif
+    } else if angle > 2. * PI {
+        angle - 2. * PI
+    } else {
+        angle
     }
 }
